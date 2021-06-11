2f8d62879f2e514bf5dc0a6935b9b6e9dbf2f0f5
kajhemmingsen <kaj.hemmingsen.85@gmail.com>
2021-06-11 14:27:13 -0400

    finished up with sign in form, added custom fields & button

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
-import HomePage from './Pages/homepage/homepage-component';
-import ShopPage from './Pages/shop/shop.component';
+import HomePage from './Pages/homepage/homepage-component.jsx';
+import ShopPage from './Pages/shop/shop-component.jsx';
+import SignInAndSignUpPage from './Pages/sign-in-and-sign-up/sign-in-and-sign-up-component.jsx';
+import Header from './Components/header/header-component.jsx';
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signin' component={SignInAndSignUpPage} />
       </Switch>
       
     </div>
