3bea6d88c914403299080d0c8c29e03a625c77fd
kajhemmingsen <kaj.hemmingsen.85@gmail.com>
2021-06-24 16:41:53 -0400

    started with firebase & google sign in

--- src/Components/sign-in/sign-in-component.jsx
+++ src/Components/sign-in/sign-in-component.jsx
@@ -4,6 +4,8 @@ import FormInput from '../form-input/form-input-component';
 import CustomButton from '../custom-button/custom-button-component';
 import './sign-in-style.scss';
 
+import { signInWithGoogle } from '../../Firebase/firebase.utils';
+
 class SignIn extends React.Component {
     constructor(props) {
         super(props);
@@ -47,6 +49,7 @@ class SignIn extends React.Component {
                         required/>
 
                     <CustomButton type='submit'>Sign In</CustomButton>
+                    <CustomButton onClick={signInWithGoogle}>{' '}Sign In With Google{' '}</CustomButton>
                 </form>
             </div>
         )
---
+++ src/Firebase/firebase.utils.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+apiKey: "AIzaSyCk5Nn5txFNT4YYS6FExsQiY7xLXhaHPyU",
+authDomain: "crown-clothing-db-b1314.firebaseapp.com",
+projectId: "crown-clothing-db-b1314",
+storageBucket: "crown-clothing-db-b1314.appspot.com",
+messagingSenderId: "892286824948",
+appId: "1:892286824948:web:19255b2c742144b68589a1",
+measurementId: "G-TR9FGM2LL8"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account'});
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
